9f387f438fa3929916dd5f270fbf6bac28329bd5
oeladawy <omnia1699@gmail.com>
2020-07-23 12:42:05 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: { 
+      fontFamily: {
+        'Lobster' : ['Lobster' , 'cursive']
+      }
+    },
+  },
+  variants: {
+    width: ["responsive", "hover", "focus"] ,
+    padding: ["responsive", "hover", "focus"] ,
+    margin: ["responsive", "hover", "focus"] ,
+    transform: ["responsive", "hover", "focus"]
+  },
+  plugins: [],
+}
